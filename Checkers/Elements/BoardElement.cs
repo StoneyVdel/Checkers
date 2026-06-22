52cb5436a379653bafb4519c402dd6aaaa4b72a0
Martynas Vaidanavičius <135174693+StoneyVdel@users.noreply.github.com>
2026-06-22 20:05:24 +0300

    selectable squares when selected a checker (#3)

---
+++ Checkers/Elements/BoardElement.cs
@@ -0,0 +1,56 @@
+﻿using SkiaSharp;
+using System.Diagnostics;
+
+namespace Checkers.Elements;
+
+public class BoardElement
+{
+    public int boardWidth = 480;
+
+    public int boardHeight = 480;
+
+    public int rectWidth => boardWidth / 8;
+
+    public int rectHeight => boardHeight / 8;
+
+    private List<BoardRect> boardRects = new List<BoardRect>();
+
+    public void AddRect(Point point)
+    {
+        var rect = new BoardRect(point, rectWidth, rectHeight);
+        boardRects.Add(rect);
+    }
+
+    public void Draw(SKCanvas canvas)
+    {
+        if (boardRects is not null)
+        {
+            boardRects.ForEach(x => x.Draw(canvas));
+        }
+    }
+
+    public void CheckSelectableRects(Point touch) 
+    {
+        if (boardRects is not null)
+        {
+            boardRects.ForEach(x => x.CheckSelectable(touch));
+        }
+    }
+
+    public void UpdateState(List<Point> points)
+    {
+        var OccupiedNum = 0;  
+        foreach (var rect in boardRects)
+        {
+            foreach(var point in points)
+            {
+                if(rect.checkOccupied(point))
+                {
+                    OccupiedNum++;
+                    break;
+                }
+            }
+        }
+        Debug.WriteLine($"occupied rect ammount {OccupiedNum}");
+    }
+}
---
+++ Checkers/Elements/BoardRect.cs
@@ -0,0 +1,60 @@
+﻿using SkiaSharp;
+using System.Diagnostics;
+
+namespace Checkers.Elements;
+
+public class BoardRect : BaseElement
+{
+    private int rectWidth { get; set; }
+
+    private int rectHeight { get; set; }
+
+    private bool isSelectable { get; set; } = false;
+
+    private bool isOccupied { get; set; } = false;
+
+    public BoardRect(Point point, int rectWidth, int rectHeight)
+    {
+        this.Point = point;
+        this.rectWidth = rectWidth;
+        this.rectHeight = rectHeight;
+        this.Color = SKColors.Black;
+    }
+
+    public void Draw(SKCanvas canvas)
+    {
+        canvas.DrawRect((float)Point.X, (float)Point.Y, rectWidth, rectHeight, new SKPaint() { Color = Color });
+    }
+
+    public void CheckSelectable(Point point)
+    {
+        var dx = (point.X - rectWidth / 2) - Point.X;
+        var dy = (point.Y - rectHeight / 2 ) - Point.Y;
+
+        isSelectable = (dx * dx) == (dy * dy);
+
+        if (isSelectable && !isOccupied) {
+            Color = SKColors.Brown;
+        }
+        else
+        {
+            Color = SKColors.Black;
+        }
+    }
+
+    public bool checkOccupied(Point point)
+    {
+        var dx = (point.X - (rectWidth /2)) - Point.X;
+        var dy = (point.Y - (rectHeight /2)) - Point.Y;
+
+        isOccupied = (dx == 0) && (dy == 0);
+
+        if (isOccupied)
+        {
+            Debug.WriteLine($"Is occupied at X: {Point.X} Y: {Point.Y} with point X: {point.X} Y: {point.Y}");
+        }
+
+        return isOccupied;
+    }
+
+}
